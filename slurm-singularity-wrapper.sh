8add6753c4f62cad1a76d19f59f986618d04539c
Dennis Klein <d.klein@gsi.de>
2025-10-23 14:38:58 +0200

    style: replace mixed tabs/spaces with consistent 2-space indentation

--- slurm-singularity-wrapper.sh
+++ slurm-singularity-wrapper.sh
@@ -3,43 +3,43 @@
 VERSION=1.0
 
 _debug() {
-        if [ "$SLURM_SINGULARITY_DEBUG" = "true" ]; then
-                echo 1>&2 "Debug: $@"
-        fi
+  if [ "$SLURM_SINGULARITY_DEBUG" = "true" ]; then
+    echo 1>&2 "Debug: $@"
+  fi
 }
 
 _error() {
-        echo 1>&2 "Error: $@"
-	exit 1
+  echo 1>&2 "Error: $@"
+  exit 1
 }
 
 run_in() {
 
-	local container="$1"
-	shift
-        test -f $container || _error "$container missing"
-        _debug "SLURM_SINGULARITY_CONTAINER=$container"
+  local container="$1"
+  shift
+  test -f $container || _error "$container missing"
+  _debug "SLURM_SINGULARITY_CONTAINER=$container"
 
-	local args="$SLURM_SINGULARITY_ARGS"
-        _debug "SLURM_SINGULARITY_ARGS=$args"
+  local args="$SLURM_SINGULARITY_ARGS"
+  _debug "SLURM_SINGULARITY_ARGS=$args"
 
-	local bind="$SLURM_SINGULARITY_BIND"
-        _debug "SLURM_SINGULARITY_BIND=$bind"
+  local bind="$SLURM_SINGULARITY_BIND"
+  _debug "SLURM_SINGULARITY_BIND=$bind"
 
-	local global="$SLURM_SINGULARITY_GLOBAL"
-        _debug "SLURM_SINGULARITY_GLOBAL=$global"
+  local global="$SLURM_SINGULARITY_GLOBAL"
+  _debug "SLURM_SINGULARITY_GLOBAL=$global"
 
-        local command="singularity $global exec --bind=$bind $args $container $@"
-        _debug "$command"
+  local command="singularity $global exec --bind=$bind $args $container $@"
+  _debug "$command"
 
-        # export the PATH and LD_LIBRARY_PATH environment variable to the container
-        #export SINGULARITYENV_PATH=$PATH
-        #export SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-        export APPTAINERENV_PREPEND_PATH=$PATH
-        export APPTAINERENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+  # export the PATH and LD_LIBRARY_PATH environment variable to the container
+  #export SINGULARITYENV_PATH=$PATH
+  #export SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+  export APPTAINERENV_PREPEND_PATH=$PATH
+  export APPTAINERENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
-        echo "Start container image $container"
-        exec $command
+  echo "Start container image $container"
+  exec $command
 }
 
 run_in "$@"
